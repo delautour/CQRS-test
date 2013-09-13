ec04d7d6d033c7f157eea55dd95be1c1e95dabda
Courtney de Lautour <clautour@thefrontiergroup.com.au>
2013-09-13 14:12:13 +0800

    Feature: Add Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CQRSTest::Application.config.secret_key_base = 'e8bdd865f0cc0d5ab477f9f1514cd1d00d6d6119c2c67fdbd1b61da1061750a78eb9a4ec954aa33b6dad75f24769d54a2404bf474e0e12882d736f504a3c6e23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CQRSTest::Application.config.session_store :cookie_store, key: '_CQRS-test_session'
